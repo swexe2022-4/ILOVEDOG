e9b21d2d4b7530b6724b9d52494cdf5ec367cf67
Ubuntu <ubuntu@ip-172-31-66-5.ec2.internal>
2022-11-09 09:16:25 +0000

    ユーザーの登録・削除・編集機能の作成

--- ILOVEDOG/app/controllers/users_controller.rb
+++ ILOVEDOG/app/controllers/users_controller.rb
@@ -1,7 +1,31 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    @user.save
+    redirect_to '/'
+  end
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to '/'
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user = User.update(name: params[:user][:name], email: params[:user][:email])
+    redirect_to '/'
   end
 end
