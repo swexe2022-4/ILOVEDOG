f131e9264021789ed604ae892b3161b8d90a4076
Ubuntu <ubuntu@ip-172-31-66-5.ec2.internal>
2022-11-23 06:50:26 +0000

    Userモデルの作り直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
+  root 'top#main'
+  get 'top/main'
+  post 'top/login'
+  get 'top/logout'
+  #get 'users/index'
+  #get 'users/new'
+  #get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
