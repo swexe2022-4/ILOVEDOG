fdcfbefe2b48cb84d601b6dc524f840f9342259f
Ubuntu <ubuntu@ip-172-31-66-5.ec2.internal>
2023-01-15 02:03:14 +0000

    1月15日朝時点でのitemのCRUD機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   #get 'users/new'
   #get 'users/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root   'items#index'
-  #root   'static_pages#home'
+  #root   'items#index'
+  root   'static_pages#home'
   get    '/signup',  to: 'users#new'
   get    '/login',    to: 'top#new'
   post   '/login',    to: 'top#create'
